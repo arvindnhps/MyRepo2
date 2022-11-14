c2800f1e5b7451e89c60390d69d43ad68eab0a7a
arvindnhps <93448820+arvindnhps@users.noreply.github.com>
2022-11-14 16:23:44 -0600

    three by arv

--- src/test/java/StepsPkg/Steps.java
+++ src/test/java/StepsPkg/Steps.java
@@ -8,7 +8,7 @@ public class Steps {
 	@Given("^User is on netbanking login page$")
 	public void user_is_on_netbanking_login_page() throws Throwable {
 	System.out.println("one");
-        System.out.println("one rep two");
+        System.out.println("one rep two three");
 
 	}
 
